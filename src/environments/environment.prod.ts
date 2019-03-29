f48c3fcdf40d3c6a243c06cab8d4437d67fa9e31
Alejandro zangari <alejandrozangari@gmail.com>
2019-03-29 10:28:35 -0300

    mi primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '12ac728600319dfe7acc8ada30f3e1f8',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
